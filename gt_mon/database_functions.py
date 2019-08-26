c75fd6209b430d01a694b4feb894fb592eb480e0
Hans Erik Fjeld <hans.erik.fjeld@embida.no>
2019-08-26 19:37:24 +0200

    Added database framework from config

--- gt_mon/database_functions.py
+++ gt_mon/database_functions.py
@@ -3,9 +3,9 @@ import serial
 import sys
 import numpy as np
 import multiprocessing
-#import ino
-import sqlite3
-from  gt_mon.helper_functions import ResultStructure
+from sqlite3 import Error
+from  helper_functions import ResultStructure
+from helper_functions import ImportConfig
 
 
 #function for converting between numpy type array to a binary blob 
@@ -20,14 +20,12 @@ def convert_array(text):
 class DatabaseFunctions(object):
     def __init__(self,ImportConfig):
         self.database_config = ImportConfig
-        self.database_name = database_config.database_name
-        
+        self.num_rows_in_config = len(self.database_config.db_rows)
+
     def init_database(self):
         db = None
         try:
-            sqlite3.register_adapter(np.array, adapt_array)
-            sqlite3.register_converter(db_row_name, convert_array)
-            db = sqlite3.connect(database_config.database , isolation_level=None, detect_types=sqlite3.PARSE_DECLTYPES)
+            db = sqlite3.connect(self.database_config.database , isolation_level=None)
             return db
         except Error as e:
             print(e)
@@ -35,15 +33,31 @@ class DatabaseFunctions(object):
 
     def create_table(self,db):
         con = db.cursor()
-        sql = 'CREATE TABLE IF NOT EXISTS ' + self.database_config.table_name 
-        sql2= ' (id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp INT, ' + self.database_config.row_name[0] + 'REAL, '+ self.database_config.row_name[1] + 'REAL ); '
-        con.execute(sql)
+        #See static/config.json
+        sql = []
+        sql.append('CREATE TABLE IF NOT EXISTS ' + self.database_config.table_name)
+        sql.append(' ( ')
+        sql.append(self.database_config.db_rows[0] + ' INTEGER PRIMARY KEY AUTOINCREMENT, ')
+        sql.append(self.database_config.db_rows[1] + ' DATETIME, ')
+        sql.append(self.database_config.db_rows[2] + ' INT , ')
+        sql.append(self.database_config.db_rows[3] + ' TEXT , ')
+        sql.append(self.database_config.db_rows[4] + ' REAL , ')
+        sql.append(self.database_config.db_rows[5] + ' TEXT , ')
+        sql.append(self.database_config.db_rows[6] + ' TEXT , ')
+        sql.append(self.database_config.db_rows[7] + ' REAL , ')
+        sql.append(self.database_config.db_rows[8] + ' REAL , ')
+        sql.append('; ')
+        print("Will create the following table:")
+        print(sql)
+        for query in sql:
+            con.execute(query)
+
         db.commit()
         con.close()
 
     def read_freshest_entry_in_database(self,db):
         con = db.cursor()
-        sql = ' SELECT * FROM ' + db_table_name + ' ORDER BY id DESC LIMIT 1 '
+        sql = ' SELECT * FROM ' + self.database_config.table_name + ' ORDER BY id DESC LIMIT 1 '
         con.execute(sql)
         readout = con.fetchone()
         con.close()
@@ -51,8 +65,28 @@ class DatabaseFunctions(object):
 
     def put_in_database(self,db,ResultStructure):
         con = db.cursor()
-        raw_dump = self.convert_array(ResultPackage.data_mean)
-        sql = 'INSERT INTO ' + self.db_table_name + ' VALUES(?,?,?) '
-        con.execute(sql,(None, ResultPackage.timestamp, raw_dump))
+        sql = []
+        sql.append('INSERT INTO ' + self.database_config.table_name + ' VALUES(')
+        for rows in self.num_rows_in_config:
+            if rows == self.num_rows_in_config:
+                sql.append('?')
+            else:
+                sql.append('?,')
+        sql.append(' ) ')
+        print('Number of rows in db :', self.num_rows_in_config)
+        print("Will generate Table :")
+        print(sql)
+        res = []
+        res.append(None)
+        res.append(ResultStructure.datetime)
+        res.append(ResultStructure.unixtime)
+        res.append(ResultStructure.ds18b20_id)
+        res.append(ResultStructure.temp)
+        res.append(self.database_config.lamps)
+        res.append(ResultStructure.light_states)
+        res.append(ResultStructure.pwm_value)
+        res.append(ResultStructure.power_consumption)
+
+        con.execute(sql,res)
         con.close()
         db.commit()
--- gt_mon/helper_functions.py
+++ gt_mon/helper_functions.py
@@ -1,5 +1,6 @@
 import json
-
+import datetime as dt
+import time as time
 
 class ImportConfig(object):
     def __init__(self, config):
@@ -7,10 +8,16 @@ class ImportConfig(object):
 
 
 class ResultStructure:
-    def __init__(self, temp_indoor, temp_outdoor, timestamp, pwm_white_led, pwm_purple_led, power_consumption ):
-        self.temp_indoor = temp_indoor
-        self.temp_outdoor = temp_outdoor
-        self.timestamp=timestamp
-        self.pwm_white_led=pwm_white_led
-        self.pwm_purple_led=pwm_purple_led
+    def __init__(self,sensor_ids, light_states,temp_indoor, temp_outdoor, pwm_white_led, pwm_purple_led, power_consumption ):
+
+        self.temp[0] = temp_indoor
+        self.temp[1] = temp_outdoor
+        self.unixtime = time.time()
+        self.datetime = dt.datetime.datetime.utcfromtimestamp(self.unixtime).strftime('%Y-%m-%dT%H:%M:%SZ')
+        self.ds18b20_id[0] = sensor_ids[0]
+        self.ds18b20_id[1] = sensor_ids[0]
+        self.light_states[0] = light_states[0]
+        self.light_states[1] = light_states[1]
+        self.pwm_value[1] = float(100 * (pwm_purple_led/255))
+        self.pwm_value[0] = float(100 * (pwm_white_led /255))
         self.power_consumption=power_consumption
